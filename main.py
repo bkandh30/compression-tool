9cfbaf85d264d357485a7eb88c343f368342cf04
bkandh30 <kandhari.bhavya20@gmail.com>
2024-09-06 15:05:53 -0700

    decompression implemented

--- main.py
+++ main.py
@@ -113,3 +113,45 @@ class HuffmanCoding:
             output.write(bytes(b))
         print("Compressed")
         return output_path
+
+    #Fundctions for decompression
+
+    def remove_padding(self, padded_encoded_text):
+        padded_info = padded_encoded_text[:8]
+        extra_padding = int(padded_info, 2)
+        padded_encoded_text = padded_encoded_text[8:] 
+        encoded_text = padded_encoded_text[:-1*extra_padding]
+        return encoded_text
+
+    def decode_text(self, encoded_text):
+        current_code = ""
+        decoded_text = ""
+        
+        for bit in encoded_text:
+            current_code += bit
+            if(current_code in self.reverse_mapping):
+                character = self.reverse_mapping[current_code]
+                decoded_text += character
+                current_code = ""
+        return decoded_text
+
+
+    def decompress(self, input_path):
+        filename, file_extension = os.path.splitext(self.path)
+        output_path = filename + "_decompressed" + ".txt"
+        
+        with open(input_path, 'rb') as file, open(output_path, 'w') as output:
+            bit_string = ""
+            byte = file.read(1)
+            while(len(byte) > 0):
+                byte = ord(byte)
+                bits = bin(byte)[2:].rjust(8, '0')
+                bit_string += bits
+                byte = file.read(1)
+            
+            encoded_text = self.remove_padding(bit_string)
+            decompressed_text = self.decode_text(encoded_text)
+            output.write(decompressed_text)
+
+        print("Decompressed")
+        return output_path
